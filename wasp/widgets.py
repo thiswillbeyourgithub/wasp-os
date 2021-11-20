2791cc9ab8d3c90e5135803f1063df961173d956
Francesco Gazzetta <fgaz@fgaz.me>
2021-11-20 17:40:52 +0100

    Make Slider.touch return True when the value changed
    Signed-off-by: Francesco Gazzetta <fgaz@fgaz.me>

--- wasp/widgets.py
+++ wasp/widgets.py
@@ -450,7 +450,9 @@ class Slider():
             v = 0
         elif v >= self._steps:
             v = self._steps - 1
+        changed = self.value != v
         self.value = v
+        return changed
 
 class Spinner():
     """A simple Spinner widget.
