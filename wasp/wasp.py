f2a3e8ea09cd2cd564ff5b7d6ffb2dc36a6514c0
thiswillbeyourgithub <github@32mail.33mail.comm>
2022-02-24 14:52:08 +0100

    reenables default apps

--- wasp/wasp.py
+++ wasp/wasp.py
@@ -167,8 +167,8 @@ class Manager():
         """Register the default applications."""
         self.register('apps.clock.ClockApp', True, no_except=True)
         #self.register('apps.steps.StepCounterApp', True, no_except=True)
-        #self.register('apps.stopwatch.StopwatchApp', True, no_except=True)
-        #self.register('apps.heart.HeartApp', True, no_except=True)
+        self.register('apps.stopwatch.StopwatchApp', True, no_except=True)
+        self.register('apps.heart.HeartApp', True, no_except=True)
 
         #self.register('apps.faces.FacesApp', no_except=True)
         self.register('apps.settings.SettingsApp', no_except=True)
