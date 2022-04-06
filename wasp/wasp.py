a473cdcf47708da714bbbcbb712291f24767fb1a
thiswillbeyourgithub <github@32mail.33mail.com>
2022-04-06 23:04:53 +0200

    add default apps 3

--- wasp/wasp.py
+++ wasp/wasp.py
@@ -169,6 +169,7 @@ class Manager():
         """Register the default applications."""
         self.register('apps.clock.ClockApp', True, no_except=True)
         #self.register('apps.steps.StepCounterApp', True, no_except=True)
+        self.register('apps.flashlight.TorchApp', True, no_except=True)
 
         self.register('apps.timer.TimerApp', False, no_except=True)
         self.register('apps.stopwatch.StopwatchApp', False, no_except=True)
@@ -179,7 +180,6 @@ class Manager():
         self.register('apps.software.SoftwareApp', False, no_except=True)
         self.register('apps.SleepTk.SleepTkApp', False, no_except=True)
         #self.register('apps.Morse.MorseApp', False, no_except=True)
-        self.register('apps.flashlight.TorchApp', False, no_except=True)
         #self.register('apps.heart.HeartApp', True, no_except=True)
         #self.register('apps.calc.CalculatorApp', False, no_except=True)
 
