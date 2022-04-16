8ba32842dc3b5d04f0ea8962d63181bffe8e61c0
thiswillbeyourgithub <github@32mail.33mail.com>
2022-04-16 10:56:15 +0200

    load disaBLE app by default

--- wasp/wasp.py
+++ wasp/wasp.py
@@ -179,6 +179,7 @@ class Manager():
         self.register('apps.settings.SettingsApp', False, no_except=True)
         self.register('apps.software.SoftwareApp', False, no_except=True)
         self.register('apps.SleepTk.SleepTkApp', False, no_except=True)
+        self.register('apps.disaBLE.DisaBLEApp', False, no_except=True)
         #self.register('apps.Morse.MorseApp', False, no_except=True)
         #self.register('apps.heart.HeartApp', True, no_except=True)
         #self.register('apps.calc.CalculatorApp', False, no_except=True)
