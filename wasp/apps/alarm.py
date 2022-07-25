8267fafe87a256ffb68cacf8974d375a08ec590b
thiswillbeyourgithub <github@32mail.33mail.com>
2022-07-25 23:41:59 +0200

    fix

--- wasp/apps/alarm.py
+++ wasp/apps/alarm.py
@@ -91,9 +91,14 @@ class AlarmApp:
         try:
             with open("alarms.txt", "r") as f:
                 alarms = f.readlines()[0].split(";")
+            if "" in alarms:
+                alarms.remove("")
             for alarm in alarms:
                 n = self.num_alarms
-                self.alarms[n][:] = map(int, alarm.split(","))
+                h, m, st = map(int, alarm.split(","))
+                self.alarms[n][0] = h
+                self.alarms[n][1] = m
+                self.alarms[n][2] = st
                 self.num_alarms += 1
         except Exception:
             pass
