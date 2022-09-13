5e0a90455944478c3e774294831b5a4ec07739a9
thiswillbeyourgithub <github@32mail.33mail.com>
2022-09-13 11:25:06 +0200

    minor

--- docs/main.py
+++ docs/main.py
@@ -1,1 +1,7 @@
-../wasp/main.py
+# SPDX-License-Identifier: LGPL-3.0-or-later
+# Copyright (C) 2020 Daniel Thompson
+
+import wasp
+# from gadgetbridge import *
+wasp._is_in_simulation = False
+wasp.system.schedule()
