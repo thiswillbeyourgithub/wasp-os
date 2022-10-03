db4008b36d38254fd1f275900b3a39736d0c8481
thiswillbeyourgithub <github@32mail.33mail.com>
2022-10-03 13:08:33 +0200

    new: reinclude gadgetbridge

--- wasp/main.py
+++ wasp/main.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2020 Daniel Thompson
 
 import wasp
-# from gadgetbridge import *
+ from gadgetbridge import *
 wasp._is_in_simulation = False
 wasp.watch.hrs.disable()
 wasp.system.schedule()
