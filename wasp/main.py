2595fc4412f7ecad0ae99933a492ab31cde10993
thiswillbeyourgithub <github@32mail.33mail.com>
2022-10-11 15:39:34 +0200

    new: wrap settings loader in try loop

--- wasp/main.py
+++ wasp/main.py
@@ -2,20 +2,26 @@
 # Copyright (C) 2020 Daniel Thompson
 
 import wasp
- from gadgetbridge import *
+from gadgetbridge import *
 
 # to differentiate from simulator
 wasp._is_in_simulation = False
 
-# disable heart rate flash if stuck
-wasp.watch.hrs.disable()
+try:
+    # disable heart rate flash if stuck
+    wasp.watch.hrs.disable()
+except:
+    pass
 
-# load previous settings
-wasp.system.notify_level = int(wasp.system.get("notify_level"))
-wasp.system.brightness = int(wasp.system.get("brightness"))
-wasp.system.hrm_freq = int(wasp.system.get("hrm_freq"))
-wasp.system.battery_unit = wasp.system.get("battery_unit")
-wasp.system.units = wasp.system.get("units")
+try:
+    # load previous settings
+    wasp.system.notify_level = int(wasp.system.get("notify_level"))
+    wasp.system.brightness = int(wasp.system.get("brightness"))
+    wasp.system.hrm_freq = int(wasp.system.get("hrm_freq"))
+    wasp.system.battery_unit = wasp.system.get("battery_unit")
+    wasp.system.units = wasp.system.get("units")
+except:
+    pass
 
 # start watch
 wasp.system.schedule()
