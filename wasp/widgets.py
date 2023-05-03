897aa484d11b2a94ea7b17a3519088af43c9b658
thiswillbeyourgithub <github@32mail.33mail.com>
2023-05-03 10:17:39 +0200

    stop changing the color of the clock when drawing it

--- wasp/widgets.py
+++ wasp/widgets.py
@@ -128,7 +128,7 @@ class Clock:
 
             draw = wasp.watch.drawable
             draw.set_font(fonts.sans18)
-            draw.set_color(wasp.system.theme('status-clock'))
+            #draw.set_color(wasp.system.theme('status-clock'))
             draw.string(t1, 52, 0, 138)
 
         self.on_screen = now
